15b1d7e471bf0ab115eaca3648e349bb3a293829
ASAD ULLAH KHALID <asadkhalid305@gmail.com>
2024-05-30 10:54:40 +0200

    feat: add turborepo with nextjs and expressjs apps with shared packages

---
+++ apps/client/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { log } from "@repo/logger";
+
+export const metadata = {
+  title: "Store | Kitchen Sink",
+};
+
+export default function Store(): JSX.Element {
+  log("Hey! This is the Store page.");
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        Store <br />
+        <span>Kitchen Sink</span>
+      </h1>
+    </div>
+  );
+}
